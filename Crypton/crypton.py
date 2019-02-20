125a415e8add2fbe8e7ecf81719bdf412ca068ec
Zafeer Hafeez <41378765+A4o4ymous@users.noreply.github.com>
2019-02-20 04:04:20 -0800

    Add files via upload

---
+++ Crypton/crypton.py
@@ -0,0 +1,167 @@
+import sys
+import os
+os.system("color 02")
+import time
+#CHECKING IF THE USER IS USING PYTHON VERSION 3 OR 2
+if (sys.version_info < (3, 0)):
+    print('''Please run it in Python 3.x version....
+Download it from this link https://www.python.org/downloads/''')
+    sys.exit()
+
+
+#TRY TO IMPORT SELENIUM
+try:
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+
+except ModuleNotFoundError:
+    print('Please install Selenium by using this command "pip install selenium"')
+    sys.exit()
+
+#VARIABLES
+name = ''
+msg = ''
+count = 0
+cmd = ''
+Pic = False
+
+#FUNCTIONS
+
+def Banner():
+    os.system('cls')
+#Don't be panic..This is just a banner...:P
+    print(b"\xff\xfe-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00\n\x00-\x00 \x00 \x00 \x00_\x00_\x00_\x00_\x00_\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00_\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00 \x00/\x00 \x00_\x00_\x00_\x00_\x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00|\x00 \x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00V\x00e\x00r\x00s\x00i\x00o\x00n\x00 \x001\x00.\x000\x00 \x00 \x00-\x00\n\x00-\x00 \x00|\x00 \x00|\x00 \x00 \x00 \x00 \x00 \x00_\x00 \x00_\x00_\x00 \x00_\x00 \x00 \x00 \x00_\x00 \x00_\x00 \x00_\x00_\x00 \x00|\x00 \x00|\x00_\x00 \x00_\x00_\x00_\x00 \x00 \x00_\x00 \x00_\x00_\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00|\x00 \x00|\x00 \x00 \x00 \x00 \x00|\x00 \x00'\x00_\x00_\x00|\x00 \x00|\x00 \x00|\x00 \x00|\x00 \x00'\x00_\x00 \x00\\\x00|\x00 \x00_\x00_\x00/\x00 \x00_\x00 \x00\\\x00|\x00 \x00'\x00_\x00 \x00\\\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00|\x00 \x00|\x00_\x00_\x00_\x00_\x00|\x00 \x00|\x00 \x00 \x00|\x00 \x00|\x00_\x00|\x00 \x00|\x00 \x00|\x00_\x00)\x00 \x00|\x00 \x00|\x00|\x00 \x00(\x00_\x00)\x00 \x00|\x00 \x00|\x00 \x00|\x00 \x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00 \x00\\\x00_\x00_\x00_\x00_\x00_\x00|\x00_\x00|\x00 \x00 \x00 \x00\\\x00_\x00_\x00,\x00 \x00|\x00 \x00.\x00_\x00_\x00/\x00 \x00\\\x00_\x00_\x00\\\x00_\x00_\x00_\x00/\x00|\x00_\x00|\x00 \x00|\x00_\x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00_\x00_\x00/\x00 \x00|\x00 \x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00|\x00_\x00_\x00_\x00/\x00|\x00_\x00|\x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00 \x00A\x00u\x00t\x00h\x00o\x00r\x00:\x00 \x00Z\x00a\x00f\x00e\x00e\x00r\x00 \x00 \x00 \x00 \x00 \x00 \x00-\x00\n\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00-\x00".decode('utf-16'))
+
+def GetInfo():
+    global name
+    global msg
+    global Pic
+    global count
+    global user
+    name = input('Target Name: ')
+    if name == "":
+        Banner()
+        GetCommands()
+    if Pic != True:
+        msg = input('Message: ')
+    count = input('Number of Count: ')
+    while not count.isdigit():
+        print('Error: Count should be number.....')
+        count = input('Number of Count: ')
+    count = int(count)
+
+
+def OpenWebDriver():
+    global driver
+    try:
+        options = webdriver.ChromeOptions();
+        options.add_argument('--log-level 3') 
+        driver = webdriver.Chrome("chromedriver.exe",service_log_path='NUL')
+        driver.get('https://web.whatsapp.com/')
+    except:
+        print('Error: Download chromedriver.exe and place it in the script\'s folder...')
+        print('       Official Source Link: http://chromedriver.chromium.org/downloads')
+        sys.exit()
+
+def SendMessage():
+    global name
+    global msg
+    global count
+    global driver
+    global user
+    try:
+        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    except:
+        print('Error: Unable to find target name.Make sure that the upper or lower casing is correct.')
+        GetInfo()
+        SendMessage()
+    user.click()
+    msg_box = driver.find_element_by_class_name('_2S1VP')
+    for i in range(count):
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_class_name('_35EW6')
+        button.click()
+    Banner()
+    print('[*] Task has Completed! {} may be angry on you :P'.format(name))
+
+
+def help():
+    print('Commands           Detail')
+    print('-------------------------------------------------------------')
+    print('wdm                wdm(Whatsapp Dos with Message) is use to -\n                   send spacied number of messages          -')
+    print('wdp                wdp(Whatsapp Dos with Picture) is use to -\n                   send spacied number of pics              -')
+    print('exit               To exit the program                      -')
+    print('about              See the version no. and author\'s detail  -')
+    print('-------------------------------------------------------------')
+
+def about():
+    Banner()
+    print('Names               Work                   Contact')
+    print('------------------------------------------------------------------------------------------------')
+    print('Zafeer              Developer+Idea         Facebook: https://www.facebook.com/We.Are.Watching.U')
+    print('                                           Github: https://github.com/A4o4ymous')
+    print('Vaibhava Mishra     Suggestions+Testing    Facebook: https://m.facebook.com/vaibhava.mishra.7965')
+    print('                                           Github: https://github.com/Vaibhava7965')
+    print('Ameer Hamza         Suggestions            Facebook: https://www.facebook.com/ahn9100')
+    print('                                           Github: https://github.com/blacklistedx')
+def SendPic():
+        global name
+        global count
+        global driver
+        global user
+        global Pic
+        for i in range(count):
+                time.sleep(.30)
+                try:
+                    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+                except:
+                    print('Error: Unable to find target name.Make sure that the upper or lower casing is correct.')
+                    GetInfo()
+                    SendPic()
+                user.click()
+                imgClip = driver.find_element_by_xpath('//div[@title="Attach"]')
+                imgClip.click()
+                time.sleep(1)
+                img = driver.find_element_by_xpath('//ul[@class="_2imug"]/li[@class="_3L0q3 _167q _1Fc0v"]//input[@type="file"]')
+                img.send_keys(os.getcwd() + "/Pic.jpg")    
+                time.sleep(1)
+                btn = driver.find_element_by_xpath('//div[@class="_3hV1n yavlE"]')
+                btn.click()
+def GetCommands():
+    global driver
+    global Pic
+    cmd = input('root@Anonymous#:~ ').upper()
+    while cmd == "":
+        cmd = input('root@Anonymous#:~ ').upper()
+    if cmd == 'WDM':
+        Pic = False
+        OpenWebDriver()
+        Banner()
+        while True:
+            GetInfo()
+            SendMessage()
+    elif cmd == 'WDP':
+        Pic = True
+        OpenWebDriver()
+        Banner()
+        while True:
+            GetInfo()
+            SendPic()
+    elif cmd == 'EXIT':
+        Banner()
+        print('See you soon..Bye!')
+        sys.exit()
+        driver.close()
+    elif cmd == 'HELP':
+        help()
+        GetCommands()
+    elif cmd == 'ABOUT':
+        about()
+        GetCommands()
+    else:
+        print('Error: Invalid Command')
+        help()
+        GetCommands()
+Banner()
+help()
+GetCommands()
